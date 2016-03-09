d5152aaaf986f13b60cd64908182472b1d2e32c6
Koustuv Sinha <koustuvsinha@hotmail.com>
2016-03-09 22:48:37 +0530

    Add cost estimator endpoint

--- scripts/uber.js
+++ scripts/uber.js
@@ -21,7 +21,12 @@ module.exports = function (robot) {
       return
     }
 
-    getLocation(res.message.text, function (err, coords) {
+    res.reply('Checking with Uber for products in the location ...')
+
+    var query = res.message.text.split(' ')
+    var loc = query.indexOf('near')
+    var location = query.slice(loc + 1)
+    getLocation(location, function (err, coords) {
       var query = base_api + 'products?latitude=' + coords.lat + '&longitude=' + coords.lng
       request.get({
         url: query,
@@ -35,22 +40,54 @@ module.exports = function (robot) {
           res.send('Minimum Fare : ' + price_details.currency_code + ' ' + price_details.minimum)
           res.send('Fare : ' + price_details.currency_code + ' ' + price_details.cost_per_distance + '/' + price_details.distance_unit)
         })
-      // res.send(JSON.stringify(body))
-      // console.log(body)
+      })
+    })
+  })
+
+  robot.respond(/calculate cost from (.*) to (.*)/i, function (res) {
+    if (!uber_server_token) {
+      res.send('Missing environment variable UBER_SERVER_TOKEN')
+    }
+    res.reply('Checking with Uber on prices ...')
+    var fromL = res.match[1]
+    var toL = res.match[2]
+    getLocation(fromL, function (err, c1) {
+      getLocation(toL, function (err, c2) {
+        var query = base_api + 'estimates/price'
+        var params = {
+          start_latitude: c1.lat,
+          start_longitude: c1.lng,
+          end_latitude: c2.lat,
+          end_longitude: c2.lng
+        }
+        request.get({
+          url: query,
+          qs: params,
+          headers: { 'Authorization': 'Token ' + uber_server_token },
+          json: true
+        }, function (err, resp, body) {
+          var prices = body.prices
+          if (prices.length > 0) {
+            prices.forEach(function (price) {
+              res.send('*' + price.display_name + '* : ' + price.estimate + ' for the distance of ' + price.distance + ' KMs')
+            })
+          } else {
+            res.send('Locations are a bit vague for Uber to understand. Can you ask again?')
+          }
+        })
       })
     })
   })
 }
 
+/**
+ * function to get lat and long from text
+ */
 function getLocation (message, cb) {
-  var query = message.split(' ')
-  var loc = query.indexOf('near')
-  var location = query.slice(loc + 1)
-  query = url + encodeURIComponent(location) + key
+  var query = url + encodeURIComponent(message) + key
   var results = ''
   request.get({url: query, json: true }, function (err, resp, body) {
     var coords = body.results[0].geometry.location
-    console.log(JSON.stringify(coords))
     cb(null, coords)
   })
 }
